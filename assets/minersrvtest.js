49fd1d8ca9f35351c38c63e674b11ce39f591a75
Jacob <46667568+jacobvd0@users.noreply.github.com>
2022-05-11 15:32:57 +0930

    Create minersrvtest.js

---
+++ assets/minersrvtest.js
@@ -0,0 +1,27 @@
+                                <script type="text/javascript">
+                                window['nitroAds'].createAd('panel-main-ad', {
+                                  "refreshLimit": 10,
+                                  "refreshTime": 30,
+                                  "renderVisibleOnly": true,
+                                  "refreshVisibleOnly": true,
+                                  "sizes": [
+                                    [
+                                      "728",
+                                      "90"
+                                    ],
+                                    [
+                                      "970",
+                                      "250"
+                                    ],
+                                    [
+                                      "970",
+                                      "90"
+                                    ]
+                                  ],
+                                  "report": {
+                                    "enabled": true,
+                                    "wording": "Report Ad",
+                                    "position": "top-right"
+                                  }
+                                });
+                                </script>
